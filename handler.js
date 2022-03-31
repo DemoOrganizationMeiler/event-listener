eb413b8332eeac5c3bf9fcf6a1933ff569f1a339
patrickmeiler <patrick.meiler@ibm.com>
2022-03-31 15:14:33 +0200

    small bugfix

--- handler.js
+++ handler.js
@@ -9,7 +9,7 @@ app.get("/health", (req, res, next) => {
   return res.status(200);
 });
 
-app.get("/github", (req, res, next) => {
+app.post("/github", (req, res, next) => {
   logger.info("received webhook fro GitHub");
   logger.debug(req.headers);
   logger.debug(req.body);
